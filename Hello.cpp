eb4a251ae27ff5ee97975f97d6a9d9c90b46a387
Koushik Kotha <koushik.kotha.2007@gmail.com>
2026-04-16 19:03:10 +0530

    Final Changes Made

--- Hello.cpp
+++ Hello.cpp
@@ -2,11 +2,7 @@
 using namespace std;
 
 int main() {
-<<<<<<< HEAD
     cout << "Hello KK, learning Git!" << endl;
-=======
-    cout << "Hello KK" << endl;
     cout << "Fixed version from stable code" << endl;
->>>>>>> test-fix
     return 0;
 }
